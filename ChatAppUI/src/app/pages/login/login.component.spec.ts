eb5e44c37fa2357e47b3693d92e01c9740c1c6aa
Tareq612625 <mdazizkhn@gmail.com>
2024-04-14 02:08:41 +0600

    message icon change

--- ChatAppUI/src/app/pages/login/login.component.spec.ts
+++ ChatAppUI/src/app/pages/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
